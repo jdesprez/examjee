ed16d1d8a6f23d84120a80841a90b7fcb35a1957
unknown <p1414331@IUTDOUAB2PC17.univ-lyon1.fr>
2015-06-24 09:19:54 +0200

    Signed-off-by: unknown <p1414331@IUTDOUAB2PC17.univ-lyon1.fr>

--- src/main/java/com/ninja_squad/geektic/service/ListGeeksService.java
+++ src/main/java/com/ninja_squad/geektic/service/ListGeeksService.java
@@ -7,11 +7,14 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ninja_squad.geektic.core.Geek;
 import com.ninja_squad.geektic.core.Genre;
+import com.ninja_squad.geektic.dao.GeekDao;
+import com.ninja_squad.geektic.dao.IGeekDao;
 
 @RestController
 @Transactional
@@ -25,20 +28,12 @@ public class ListGeeksService
 //		return new ListGeeksDisplay();
 //	}
 
+	@Autowired
+	public GeekDao geekDao;
 
 	@RequestMapping(method = GET)
 	public List<Geek> printList()
-	{
-		Geek g1 = new Geek("Nom","Prenom","toto@gmail.com", Genre.HOMME);
-		Geek g2 = new Geek("Quelqun","Dautre","personne@gmail.com", Genre.HOMME);
-		Geek g3 = new Geek("toto","tata","tototata@gmail.com", Genre.FEMME);
-		
-		ArrayList<Geek> listRet = new ArrayList<Geek>();
-		
-		listRet.add(g1);
-		listRet.add(g2);
-		listRet.add(g3);
-		
-		return listRet;
+	{	
+		return geekDao.findAll();
 	}
 }
